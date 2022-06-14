b7b8da344718437eab21d365c1d87cb781e2058d
tzn <790453078@qq.com>
2022-06-14 15:55:05 +0800

    实现搜索全球城市数据功能。

---
+++ app/src/main/java/com/sunnyweather/android/logic/Repository.kt
@@ -0,0 +1,30 @@
+package com.sunnyweather.android.logic
+
+
+import androidx.lifecycle.liveData
+import com.sunnyweather.android.logic.model.Place
+import com.sunnyweather.android.logic.network.SunnyWeatherNetwork
+import kotlinx.coroutines.Dispatchers
+import kotlin.coroutines.CoroutineContext
+
+object Repository {
+    fun searchPlaces(query: String) = fire(Dispatchers.IO) {
+        val placeResponse = SunnyWeatherNetwork.searchPlace(query)
+        if (placeResponse.status == "ok") {
+            val place = placeResponse.places
+            Result.success(place)
+        } else {
+            Result.failure(RuntimeException("response status is${placeResponse.status}"))
+        }
+    }
+
+    private fun <T> fire(context: CoroutineContext, block: suspend () -> Result<T>) =
+        liveData<Result<T>>(context) {
+            val result = try {
+                block()
+            } catch (e: Exception) {
+                Result.failure<T>(e)
+            }
+            emit(result)
+        }
+}
---
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceFragment.kt
@@ -0,0 +1,69 @@
+package com.sunnyweather.android.ui.place
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.sunnyweather.android.R
+
+class PlaceFragment : Fragment() {
+    private val viewModel by lazy {
+        ViewModelProvider(this).get(PlaceViewModel::class.java)
+    }
+    private lateinit var adapter: PlaceAdapter
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var searchPlaceEdit: EditText
+    private lateinit var bgImageView: ImageView
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view: View = inflater.inflate(R.layout.fragment_place, container, false)
+        recyclerView = view.findViewById(R.id.recyclerView)
+        searchPlaceEdit = view.findViewById(R.id.searchPlaceEdit)
+        bgImageView = view.findViewById(R.id.bgImageView)
+        return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val layoutManager = LinearLayoutManager(activity)
+        recyclerView.layoutManager = layoutManager
+        adapter = PlaceAdapter(this, viewModel.placeList)
+        recyclerView.adapter = adapter
+        searchPlaceEdit.addTextChangedListener { editable ->
+            val content = editable.toString()
+            if (content.isNotEmpty()) {
+                viewModel.searchPlace(content)
+            } else {
+                recyclerView.visibility = View.GONE
+                bgImageView.visibility = View.VISIBLE
+                viewModel.placeList.clear()
+                adapter.notifyDataSetChanged()
+            }
+        }
+        viewModel.placeLiveData.observe(this, Observer { result ->
+            val places = result.getOrNull()
+            if (places != null) {
+                recyclerView.visibility = View.VISIBLE
+                bgImageView.visibility = View.GONE
+                viewModel.placeList.clear()
+                viewModel.placeList.addAll(places)
+                adapter.notifyDataSetChanged()
+            } else {
+                Toast.makeText(activity, "未能查询到任何地点", Toast.LENGTH_SHORT).show()
+                result.exceptionOrNull()?.printStackTrace()
+            }
+        })
+    }
+}
